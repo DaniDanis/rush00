e8a313a0e4098310a370ad1f547824dbd36ab2ed
Chrystian Natanael Magalhaes Silva Nascimento <cnatanae@c2r7p2.42sp.org.br>
2023-08-19 17:26:38 -0300

    modificações de semantica e identação rush04

--- ex00/rush04.c
+++ ex00/rush04.c
@@ -14,46 +14,39 @@
 
 char	ft_putchar(char c);
 
-void	ft_write_line(int cord_x, int cord_y, int x, int y)
+void	ft_write_square(int x, int y)
 {
-	while (cord_x <= x)
+	int	cord_x;
+	int	cord_y;
+
+	cord_y = 1;
+	while (cord_y <= y)
 	{
-		if ((cord_x == 1 && cord_y == 1) || (cord_x == x && cord_y == y))
+		cord_x = 1;
+		while (cord_x <= x)
 		{
-			ft_putchar('A');
+			if ((cord_x == 1 && cord_y == 1) || (cord_x == x && cord_y == y))
+				ft_putchar('A');
+			else if ((cord_y == 1 && cord_x == x)
+				|| (cord_x == 1 && cord_y == y))
+				ft_putchar('C');
+			else if (cord_y == 1 || cord_x == 1 || cord_y == y || cord_x == x)
+				ft_putchar('B');
+			else
+				ft_putchar(' ');
+			cord_x++;
 		}
-		else if ((cord_y == 1 && cord_x == x) || (cord_x == 1 && cord_y == y))
-		{
-			ft_putchar('C');
-		}
-		else if (cord_y == 1 || cord_x == 1 || cord_y == y || cord_x == x)
-		{
-			ft_putchar('B');
-		}
-		else
-		{
-			ft_putchar(' ');
-		}
-		cord_x++;
+		cord_y++;
+		ft_putchar('\n');
 	}
 }
 
 void	rush(int x, int y)
 {
-	int	cord_x;
-	int	cord_y;
-
 	if (x <= 0 || y <= 0)
 	{
 		write(1, "Negative numbers and 0 are not allowed.", 39);
 		return ;
 	}
-	cord_y = 1;
-	while (cord_y <= y)
-	{
-		cord_x = 1;
-		ft_write_line (cord_x, cord_y, x, y);
-		cord_y++;
-		ft_putchar('\n');
-	}
+	ft_write_square (x, y);
 }
