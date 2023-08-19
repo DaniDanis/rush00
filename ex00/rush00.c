f203afc100f2c59269543efc7ba47c65414961cc
Chrystian Natanael Magalhaes Silva Nascimento <cnatanae@c2r7p2.42sp.org.br>
2023-08-19 17:19:03 -0300

    exercicio rush00 perfeitamente funcional

---
+++ ex00/rush00.c
@@ -0,0 +1,52 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   rush00.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cnatanae <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/19 15:06:39 by cnatanae          #+#    #+#             */
+/*   Updated: 2023/08/19 16:59:30 by cnatanae         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+char	ft_putchar(char c);
+
+void	ft_write_square(int x, int y)
+{
+	int	cord_x;
+	int	cord_y;
+
+	cord_y = 1;
+	while (cord_y <= y)
+	{
+		cord_x = 1;
+		while (cord_x <= x)
+		{
+			if ((cord_x == 1 && cord_y == 1) || (cord_x == x && cord_y == 1)
+				|| (cord_y == y && cord_x == 1) || (cord_x == x && cord_y == y))
+				ft_putchar('o');
+			else if (cord_y == 1 || cord_y == y)
+				ft_putchar('-');
+			else if (cord_x == 1 || cord_x == x)
+				ft_putchar('|');
+			else
+				ft_putchar(' ');
+			cord_x++;
+		}
+		cord_y++;
+		ft_putchar('\n');
+	}
+}
+
+void	rush00(int x, int y)
+{
+	if ((x <= 0) || (y <= 0))
+	{
+		write(1, "Negative numbers and 0 are not allowed.", 39);
+		return ;
+	}	
+	ft_write_square(x, y);
+}
