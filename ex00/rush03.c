2a32b1bf445755579e4e3a31dec11ba265d016f2
gcouto-f <gcforte@me.com>
2023-08-20 14:15:09 -0300

    rush03.c

---
+++ ex00/rush03.c
@@ -0,0 +1,52 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   rush03.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gcouto-f <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/20 14:10:13 by gcouto-f          #+#    #+#             */
+/*   Updated: 2023/08/20 14:10:17 by gcouto-f         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+char	ft_putchar(char c);
+
+void	ft_write_square(int x, int y)
+{
+	int	cord_x;
+	int	cord_y;
+
+	cord_y = 1;
+	while (cord_y <= y)
+	{
+		cord_x = 1;
+		while (cord_x <= x)
+		{
+			if ((cord_x == 1 && cord_y == 1) || (cord_x == 1 && cord_y == y))
+				ft_putchar('A');
+			else if ((cord_y == y && cord_x == x)
+				|| (cord_x == x && cord_y == 1))
+				ft_putchar('C');
+			else if (cord_y == 1 || cord_x == 1 || cord_y == y || cord_x == x)
+				ft_putchar('B');
+			else
+				ft_putchar(' ');
+			cord_x++;
+		}
+		cord_y++;
+		ft_putchar('\n');
+	}
+}
+
+void	rush(int x, int y)
+{
+	if (x <= 0 || y <= 0)
+	{
+		write(1, "Negative numbers and 0 are not allowed.", 39);
+		return ;
+	}
+	ft_write_square (x, y);
+}
